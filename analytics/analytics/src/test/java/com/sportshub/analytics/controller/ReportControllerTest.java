b949f973ffb1957f1ab689c490e24d74391c29c1
Mika <mika.bjelobrk@eurobit.ba>
2026-05-07 10:28:34 +0200

    Fixed Import

--- analytics/analytics/src/test/java/com/sportshub/analytics/controller/ReportControllerTest.java
+++ analytics/analytics/src/test/java/com/sportshub/analytics/controller/ReportControllerTest.java
@@ -9,7 +9,7 @@ import com.sportshub.analytics.model.Report;
 import com.sportshub.analytics.service.ReportService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
